fa77d01dbc5166cd2478c3c1f426ffda8cda833b
Aleksandra Petrova <petrowa.alex@yandex.com>
2023-02-10 20:16:42 +0300

    Second question: added comments

--- second_question.py
+++ second_question.py
@@ -1,3 +1,15 @@
+'''Какие ты видишь проблемы в следующем коде? 
+Как его следует исправить? 
+Исправь ошибку и перепиши код с использованием типизации'''
+
+'''Ошибка в lambda. Чтобы исправить добавила в lambda переменную step,
+добавленная переменная перехватывает текущее значение step и
+передает его в lambda.
+'''
+
+'''Для проверки добавила тестовый обработчик step_plus_one.
+'''
+
 from collections.abc import Callable
 
 def create_handlers(callback: Callable) -> list[Callable]:
@@ -18,5 +30,5 @@ def step_plus_one(step: int):
     print(f'{step} + 1 = {step + 1}')
 
 handlers = create_handlers(step_plus_one)
-print(execute_handlers(handlers))
+execute_handlers(handlers)
 
